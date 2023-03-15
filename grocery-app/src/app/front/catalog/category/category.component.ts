46bdb3b7f5debc67c753e464365fbc1451dcde18
shahvijeet <vijeetshah1408@gmail.com>
2023-03-15 12:57:59 +0530

    15-03-2023 01:00

--- grocery-app/src/app/front/catalog/category/category.component.ts
+++ grocery-app/src/app/front/catalog/category/category.component.ts
@@ -7,16 +7,16 @@ import { Component } from '@angular/core';
 })
 export class CategoryComponent {
   array:any=[
-    {catagory:'vagetables',cat_name:'redish',quantity:500,price:2,img:"strawberry.png"},
-    {catagory:'vagetables',cat_name:'potato',quantity:500,price:2,img:'orange.png'},
-    {catagory:'Fruits',cat_name:'redish',quantity:500,price:2,img:'banana.png'},
-    {catagory:'vagetables',cat_name:'potato',quantity:500,price:2,img:'potato.png'},
-    {catagory:'Fruits',cat_name:'redish',quantity:500,price:2,img:'apple.png'},
+    {catagory:'vagetables',cat_name:'potato',quantity:500,price:2,img:"strawberry.png"},
+    {catagory:'vagetables',cat_name:'tomato',quantity:500,price:2,img:'orange.png'},
+    {catagory:'fruits',cat_name:'orange',quantity:500,price:2,img:'banana.png'},
+    {catagory:'vagetables',cat_name:'chilly',quantity:500,price:2,img:'potato.png'},
+    {catagory:'fruits',cat_name:'apple',quantity:500,price:2,img:'apple.png'},
     ]
-    filteredPeople = this.array;
+    filteredcatagory = this.array;
 
-    filterPeopleByAge(category: string) {
-      this.filteredPeople = this.array.filter((array: { category: string; }) => array.category >= category);
+    filtercatagorybyitem(catagory: string) {
+      this.filteredcatagory = this.array.filter((array: { catagory: string; }) => array.catagory == catagory);
     }
 }
 
